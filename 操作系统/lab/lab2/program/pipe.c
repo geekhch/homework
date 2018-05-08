73f699df8c8a671f90847e8455546f98f810aaac
geekhch <709531006@qq.com>
2018-05-08 18:17:51 +0800

    fds

--- 操作系统/lab/lab2/program/pipe.c
+++ 操作系统/lab/lab2/program/pipe.c
@@ -31,27 +31,27 @@ int main(int argc, char *argv[])
     perror("Fork failed");
     exit(errno);
   }
-  if (!pid[0])
+  if (!pid[0]) //子进程1
   {
-    close(pipe_fd[0]);
-    dup2(pipe_fd[1],1);
-    close(pipe_fd[1]);
-
+    close(pipe_fd[0]); //fildes[0]元素作为管道的输入描述符，
+    /*将管道的写描述符复制给标准输出,然后关闭*/
+    dup2(pipe_fd[1], 1); //fildes[1]元素作为管道的输出描述符。
+    close(pipe_fd[1]);   //关闭输出
     execvp(prog1_argv[0], prog1_argv);
   }
   
-  if (pid[0])
+  if (pid[0]) //父进程
   {
     if ((pid[1]=fork())<0)
     {
       perror("Fork failed");
       exit(errno);
     }
-    if (!pid[1])
+    if (!pid[1]) //父进程
     {
       close(pipe_fd[1]);
-      dup2(pipe_fd[0],0);
-      close(pipe_fd[0]);
+      dup2(pipe_fd[0],0); 
+      close(pipe_fd[0]);   
       
       execvp(prog2_argv[0],prog2_argv);
     }
@@ -62,3 +62,5 @@ int main(int argc, char *argv[])
     printf("Done waiting for more.\n");
   }  
 }
+
+//filed[0]输入端, 0键盘输入
--- 操作系统/lab/lab2/program/test.c
+++ 操作系统/lab/lab2/program/test.c
@@ -1,7 +1,58 @@
+#include <string.h>
 #include <stdio.h>
+#include <errno.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-	puts("hello");
-	return 0;
+	int status;
+	int pid[2];
+	int pipe_fd[4];
+
+
+	if (pipe(pipe_fd) < 0)  //创建管道
+	{
+		perror("pipe failed");
+		exit(errno);
+	}
+	if ((pid[0] = fork()) < 0) //child 1
+	{
+		perror("Fork 1 failed");
+		exit(errno);
+	}
+
+	if (!pid[0]) //子进程1
+	{
+		close(pipe_fd[0]); //fildes[0]元素作为管道的输入描述符，
+		/*将管道的写描述符复制给标准输出,然后关闭*/
+		dup2(pipe_fd[1], pipe_fd[2]); //fildes[1]元素作为管道的输出描述符。
+		close(pipe_fd[1]);   //关闭输出
+		execvp("echo '子进程1运行啦'");
+	}
+
+	if (pid[0]) //父进程
+	{
+		if ((pid[1] = fork()) < 0)
+		{
+			perror("Fork failed");
+			exit(errno);
+		}
+
+		if (!pid[1]) //子进程2
+		{
+			waitpid(pid[0], &status, 0);
+			dup("pipe_fd[2]", 0); //fildes[0]元素作为管道的输入描述符，
+			/*将管道的写描述符复制给标准输出,然后关闭*/
+			// dup2(pipe_fd[1], 1); //fildes[1]元素作为管道的输出描述符。
+			close(pipe_fd[2]); //关闭输出
+		}
+		
+		if(pid[1])
+		{
+			dup("pipe_fd[2]", 1);
+			waitpid(pid[1], &status, 0);
+			printf("Done waiting for more.\n");
+		}
+	}
 }
+
+//filed[0]输入端, 0键盘输入
